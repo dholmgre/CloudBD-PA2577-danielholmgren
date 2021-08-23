29e11890c0d7ccc7bd106e9c4f525ceb483d106b
dholmgre <daniel_holmgren@live.se>
2021-08-23 17:55:24 +0200

    provisioning for digital ocean

---
+++ Assignments/provisioning-cloud/README.md
@@ -0,0 +1,3 @@
+vagrant up --provider virtualbox
+vagrant up --provider digital_ocean
+  
---
+++ Assignments/provisioning-cloud/environments/test/manifests/default.pp
@@ -0,0 +1,54 @@
+node 'appserver' {
+	include apt_get_update
+	include nodejs
+}
+
+node 'dbserver' {
+	include apt_get_update
+	include mysql
+}
+
+
+class apt_get_update{
+	exec {"apt_get_update":
+		command => 'sudo apt-get update',
+		provider => shell,
+	}
+}
+
+
+class nodejs{
+	
+	exec {"prerequisite":
+			command => 'curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -',
+			provider => shell,
+	}
+	
+	exec {"nodejs":
+		command => 'sudo apt-get install -y nodejs',
+		provider => shell,
+		require => [
+			Exec["apt_get_update"],
+			Exec["prerequisite"],
+		],
+	}
+}
+
+
+class mysql{
+
+	exec {"mysql":
+		command => 'sudo apt-get install -y mysql-server',
+		provider => shell,
+		require => Exec["apt_get_update"],
+	}
+
+	exec {"mysql-set-pw":
+		command => 'echo mysql-server mysql-server/root_password password Passw0rd! | sudo debconf-set-selections',
+		provider => shell,
+		require => [
+			Exec["apt_get_update"],
+			Exec["mysql"],
+		],
+	}
+}
